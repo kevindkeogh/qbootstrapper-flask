6f14dd1e1dc500c2bd6b729491c2145253421475
Kevin Keogh <kevin.d.keogh@gmail.com>
2017-04-09 12:38:49 -0400

    add HTML5 fields

--- qbflask/forms.py
+++ qbflask/forms.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 import wtforms as wtf
+import wtforms.fields.html5 as wtffields
 
 INSTRUMENT_TYPES = [('OISSwap', 'OIS Swap'), ('OISCashRate', 'OIS Cash Rate')]
 
 class Instrument(wtf.Form):
     instrument_type = wtf.SelectField('Instrument', choices=INSTRUMENT_TYPES)
-    expiry = wtf.DateField('Expiry', '%Y-%m-%d')
-    rate = wtf.DecimalField('Rate')
+    expiry = wtffields.DateField('Expiry', '%Y-%m-%d')
+    rate = wtffields.DecimalField('Rate')
 
 class InstrumentList(wtf.Form):
     instruments = wtf.FieldList(wtf.FormField(Instrument), min_entries=3)
