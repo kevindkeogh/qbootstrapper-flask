059462b8b67833357718f70920ee2c6503d8a9c5
Kevin Keogh <kevin.d.keogh@gmail.com>
2017-04-10 23:57:05 -0400

    jslint ajax-calls.js

--- qbflask/static/js/ajax-calls.js
+++ qbflask/static/js/ajax-calls.js
@@ -4,15 +4,15 @@ $(document).ready(function () {
 
     $(instsForm).submit(function (e) {
         e.preventDefault();
-		var instsData = $(instsForm).serializeArray();
-		$.ajax({
-			type: "POST",
-			url: "/curve",
-			data: JSON.stringify(instsData),
-			contentType: "application/json;charst=UTF-8",
-			success: function(result) {
-				console.log(result);
-			}});
-		return false;
+        var instsData = $(instsForm).serializeArray();
+        $.ajax({
+            type: "POST",
+            url: "/curve",
+            data: JSON.stringify(instsData),
+            contentType: "application/json;charst=UTF-8",
+            success: function(result) {
+                console.log(result);
+            }});
+        return false;
     });
 });
