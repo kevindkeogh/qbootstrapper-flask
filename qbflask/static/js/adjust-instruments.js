0c4aaf93521663d8929edf6a058d7024539b34fd
Kevin Keogh <kevin.d.keogh@gmail.com>
2017-04-12 22:17:37 -0400

    remove limit on number of instruments

--- qbflask/static/js/adjust-instruments.js
+++ qbflask/static/js/adjust-instruments.js
@@ -1,27 +1,21 @@
 $(document).ready(function () {
     "use strict";
-    var maxFields = 30;
     var wrapper = $(".input-instruments");
     var addButton = $(".add-instrument-button");
 
-    var index = 1;
-
     $(addButton).click(function (e) {
         e.preventDefault();
-        if (index < maxFields) { // not reached max insts
-            index += 1;
-            var lastRow = $("#instruments-table tr:last");
-            var newRow = lastRow.clone(true);
-            var rowNum = parseInt(lastRow.attr("id").replace(/[^0-9.]/g, ""));
-            rowNum += 1;
-            newRow.attr("id", "inst-" + rowNum);
-            newRow.children().each(function () {
-                var td = this.children[0];
-                td.name = td.name.replace(/\d+/g, rowNum);
-                td.id = td.id.replace(/\d+/g, rowNum);
-            });
-            lastRow.after(newRow);
-        }
+        var lastRow = $("#instruments-table tr:last");
+        var newRow = lastRow.clone(true);
+        var rowNum = parseInt(lastRow.attr("id").replace(/[^0-9.]/g, ""));
+        rowNum += 1;
+        newRow.attr("id", "inst-" + rowNum);
+        newRow.children().each(function () {
+            var td = this.children[0];
+            td.name = td.name.replace(/\d+/g, rowNum);
+            td.id = td.id.replace(/\d+/g, rowNum);
+        });
+        lastRow.after(newRow);
     });
 
     $(wrapper).on("click", ".remove-instrument-button", function (e) {
