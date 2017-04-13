ecb365fe630b05dad462f6d42e950027b0e335cc
Kevin Keogh <kevin.d.keogh@gmail.com>
2017-04-12 22:11:08 -0400

    JSHint cleanup

--- qbflask/static/js/ajax-calls.js
+++ qbflask/static/js/ajax-calls.js
@@ -21,18 +21,18 @@ $(document).ready(function () {
 function displayDFs (results) {
 	"use strict";
 	var outputTable = $("#output-table");
-	$(outputTable).empty() // Remove all child elements
-						   // in case this is not the first run
-	var headerRow = $("<thead><tr><th>Dates</th>"
-			          + "<th>Discount Factors</th></tr></thead>");
+	$(outputTable).empty(); // Remove all child elements
+						    // in case this is not the first run
+	var headerRow = $("<thead><tr><th>Dates</th>" +
+            "<th>Discount Factors</th></tr></thead>");
 	$(headerRow).appendTo(outputTable);
 
 	var numRows = results.dates.length;
 	for (var i=0; i<numRows; i++) {
 		var date = results.dates[i];
 		var df = results.dfs[i].toString().substring(0, 12);
-		outputTable.append("<tr><td class=\"date\">" + date +          "</td>"
-						 +     "<td class=\"discount-factor\">" + df + "</td>"
-						 + "</tr>");
-	};
+		outputTable.append("<tr><td class=\"date\">" + date +          "</td>" +
+						       "<td class=\"discount-factor\">" + df + "</td>" +
+						   "</tr>");
+	}
 }
